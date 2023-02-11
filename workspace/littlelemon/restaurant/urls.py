31087a0b5a77f8bffce3b5267bd5af5a5747f6d1
Noman-Memon <96913980+Noman-Memon@users.noreply.github.com>
2023-02-11 18:41:12 +0500

    set urls for booking model

--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,7 +1,13 @@
 #define URL route for index() view
 from django.urls import path
 from . import views
+# from rest_framework import routers
+
+# routers.DefaultRouter.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', views.BookingViewSet.as_view()),
+    ]
